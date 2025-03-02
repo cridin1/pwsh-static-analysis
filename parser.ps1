8009b7823d8bc6ef4c965173b876c3eb479139d8
christianmarescalco <marescalcoc@gmail.com>
2025-03-03 00:47:32 +0100

    updated parser.ps1

--- parser.ps1
+++ parser.ps1
@@ -3,5 +3,5 @@ param(
     [string]$output_path
 )
 
-$array = Invoke-ScriptAnalyzer $script_path -Settings PSGallery -Recurse
+$array = Invoke-ScriptAnalyzer $script_path -Settings PSGallery -Recurse -Verbose 
 $array | Export-Csv -Path $output_path -NoTypeInformation
